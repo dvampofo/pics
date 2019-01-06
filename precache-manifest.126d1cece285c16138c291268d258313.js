4c7e8d2374ddcf049eb7daacf0a782258cc6f8c0
David Ampofo <davidamp@my.yorku.ca>
2019-01-06 18:44:34 -0500

    Updates

---
+++ precache-manifest.126d1cece285c16138c291268d258313.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "9e0d4f6ad25f02f07e2d",
+    "url": "/pics/static/js/runtime~main.9e0d4f6a.js"
+  },
+  {
+    "revision": "9a4eab694a9a3bc2a106",
+    "url": "/pics/static/js/main.9a4eab69.chunk.js"
+  },
+  {
+    "revision": "102c3bf24ee7bc42981e",
+    "url": "/pics/static/js/1.102c3bf2.chunk.js"
+  },
+  {
+    "revision": "9a4eab694a9a3bc2a106",
+    "url": "/pics/static/css/main.c227e1c7.chunk.css"
+  },
+  {
+    "revision": "3917f3aac16cf1f6a8feb8a9e44cdfc7",
+    "url": "/pics/index.html"
+  }
+];
