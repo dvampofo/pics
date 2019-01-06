fdd6c9c8eea285489886fcc6f36b51c3114de151
David Ampofo <davidamp@my.yorku.ca>
2019-01-05 19:04:16 -0500

    First commit

---
+++ src/components/SearchBar.js
@@ -0,0 +1,32 @@
+import React from "react";
+
+class SearchBar extends React.Component {
+  state = { term: "" };
+
+  // Prevent form from submitting itself and then refreshing the page
+  // Arrow function will always make sure that the value `this` is
+  // always equal to our instance of the SearchBar
+  onFormSubmit(event) {
+    event.preventDefault();
+    console.log(this.state.term);
+  }
+
+  render() {
+    return (
+      <div className="ui segment">
+        <form onSubmit={event => this.onFormSubmit(event)} className="ui form">
+          <div className="field">
+            <label>Image Search</label>
+            <input
+              type="text"
+              value={this.state.term}
+              onChange={e => this.setState({ term: e.target.value })}
+            />
+          </div>
+        </form>
+      </div>
+    );
+  }
+}
+
+export default SearchBar;
